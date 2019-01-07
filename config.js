a32596462414f340f7bb4c700f255bda44987c48
Daniel Ruiz <xiscodann@gmail.com>
2019-01-06 21:42:52 -0500

    version 3.0

--- config.js
+++ config.js
@@ -1,19 +1,22 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard(){
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
     this.classList.add('flip');
 
     if(!hasFlippedCard){
         hasFlippedCard = true;
         firstCard = this;
-    }else{
-        hasFlippedCard = false;
-        secondCard = this;
-        checkForMatch();
+        return;
     }
+    secondCard = this;
+    checkForMatch();
 }
 function checkForMatch(){
     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
@@ -22,11 +25,24 @@ function checkForMatch(){
 function disableCards(){
     firstCard.style.pointerEvents = "none";
     secondCard.style.pointerEvents = "none";
+    resetBoard();
 }
 function unflipCards(){
+    lockBoard = true;
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+        resetBoard();
     }, 1500);
 }
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+(function shuffle(){
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
 cards.forEach(card => card.addEventListener('click', flipCard));
